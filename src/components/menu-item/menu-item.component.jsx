3d957bb9010d30f1611c0dd9e16c861be73c5e86
Nickan <Nickan.bagherpour@gmail.com>
2021-11-13 22:11:46 +0330

    feat: add routing with react-router, then create new routes for our existing homepage and our new hats page

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,20 +1,27 @@
 import React from "react";
-
+import { withRouter } from "react-router";
 import "./menu-item.styles.scss";
 
-const MenuItem = ({ title, imageUrl, size }) => (
-  <div className={`${size} menu-item`}>
-    <div
-      className="background-image"
-      style={{
-        backgroundImage: `url(${imageUrl})`,
-      }}
-    />
-    <div className="content">
-      <h1 className="title">{title}</h1>
-      <span className="subtitle">SHOP NOW</span>
-    </div>
-  </div>
-);
+const MenuItem = ({ title, imageUrl, size, linkUrl, history, match }) => {
+  const handleMenuItemClick = () => {
+    history.push(`${match.url}${linkUrl}`);
+  };
 
-export default MenuItem;
+  return (
+    //history & match are related to withRouter HOC [Higher Order Component]
+    <div className={`${size} menu-item`} onClick={handleMenuItemClick}>
+      <div
+        className="background-image"
+        style={{
+          backgroundImage: `url(${imageUrl})`,
+        }}
+      />
+      <div className="content">
+        <h1 className="title">{title}</h1>
+        <span className="subtitle">SHOP NOW</span>
+      </div>
+    </div>
+  );
+};
+
+export default withRouter(MenuItem);
