e8b238e61822edb6ff8610c7efed104f8628e38f
Nickan <Nickan.bagherpour@gmail.com>
2021-11-14 10:36:10 +0330

    feat: work on AuthPage - create SignIn component, FormInput & CustomButton components ...

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 
 import Header from './components/header/header.component.jsx';
+import AuthPage from "./pages/auth/auth.component";
 import HomePage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shop/shop.component";
 
@@ -14,6 +15,7 @@ function App() {
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route path="/shop" component={ShopPage} />
+        <Route path="/auth" component={AuthPage} />
       </Switch>
     </BrowserRouter>
   );
