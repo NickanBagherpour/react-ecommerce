691684dbfc7ea8fec9827f3ddac1df859855efb5
Nickan <Nickan.bagherpour@gmail.com>
2021-11-20 21:05:40 +0330

    feat: update cart redux with add item action [basic implementation]

--- src/redux/cart/cart.actions.js
+++ src/redux/cart/cart.actions.js
@@ -1,6 +1,11 @@
-import {CartActionTypes} from "./cart.types";
+import { CartActionTypes } from "./cart.types";
 
 export const toggleCartHidden = () => ({
   type: CartActionTypes.TOGGLE_CART_HIDDEN,
   //payload no need ...
+});
+
+export const addItem = (item) => ({
+  type: CartActionTypes.ADD_ITEM,
+  payload: item,
 });
