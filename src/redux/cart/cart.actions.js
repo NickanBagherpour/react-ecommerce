1ac64536ed97f0d10e8a313cb68b88aa220e2dea
Nickan <Nickan.bagherpour@gmail.com>
2021-11-22 23:34:50 +0330

    feat: add clear,add,remove item functionality to checkout item component

--- src/redux/cart/cart.actions.js
+++ src/redux/cart/cart.actions.js
@@ -1,11 +1,21 @@
-import { CartActionTypes } from "./cart.types";
+import {CartActionTypes} from "./cart.types";
 
 export const toggleCartHidden = () => ({
-  type: CartActionTypes.TOGGLE_CART_HIDDEN,
-  //payload no need ...
+    type: CartActionTypes.TOGGLE_CART_HIDDEN,
+    //payload no need ...
 });
 
-export const addItem = (item) => ({
-  type: CartActionTypes.ADD_ITEM,
-  payload: item,
+export const addItem = item => ({
+    type: CartActionTypes.ADD_ITEM,
+    payload: item,
+});
+
+export const removeItem = item => ({
+    type: CartActionTypes.REMOVE_ITEM,
+    payload: item
+});
+
+export const clearItem = item => ({
+    type: CartActionTypes.CLEAR_ITEM,
+    payload: item
 });
--- src/redux/cart/cart.utils.js
+++ src/redux/cart/cart.utils.js
@@ -1,11 +1,31 @@
 export const addItemToCart = (cartItems, newItem) => {
-  const existingCartItem = cartItems.find((item) => item.id === newItem.id);
+    const existingCartItem = cartItems.find((item) => item.id === newItem.id);
 
-  if (existingCartItem) {
-    return cartItems.map((item) =>
-      item.id === newItem.id ? { ...item, quantity: item.quantity + 1 } : item
+    if (existingCartItem) {
+        return cartItems.map((item) =>
+            item.id === newItem.id ? {...item, quantity: item.quantity + 1} : item
+        );
+    }
+
+    return [...cartItems, {...newItem, quantity: 1}];
+};
+
+export const removeItemFromCart = (cartItems, cartItemToRemove) => {
+    const existingCartItem = cartItems.find(
+        cartItem => cartItem.id === cartItemToRemove.id
     );
-  }
 
-  return [...cartItems, { ...newItem, quantity: 1 }];
+    if (existingCartItem.quantity === 1) {
+        return cartItems.filter(cartItem => cartItem.id !== cartItemToRemove.id); //or return clearItemFromCart(cartItems, cartItemToRemove)
+    }
+
+    return cartItems.map(cartItem =>
+        cartItem.id === cartItemToRemove.id
+            ? {...cartItem, quantity: cartItem.quantity - 1}
+            : cartItem
+    );
+};
+
+export const clearItemFromCart = (cartItems, selectedItem) => {
+    return cartItems.filter(item => item.id !== selectedItem.id);
 };
