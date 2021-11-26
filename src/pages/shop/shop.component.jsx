8246299803bcdaff6a5a5ae3b035d6216c6ab6a9
Nickan <Nickan.bagherpour@gmail.com>
2021-11-26 09:31:17 +0330

    chore: bumped version number to 0.1.0

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -5,9 +5,6 @@ import CollectionsOverView from "../../components/collections-overview/collectio
 import CollectionPage from "../collection/collection.component";
 
 const ShopPage = ({match}) => {
-
-    console.log(match);
-
     return (
         <div className="shop-page">
             <Route exact path={`${match.path}`} component={CollectionsOverView}/>
