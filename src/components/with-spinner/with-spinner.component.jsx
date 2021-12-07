de63c3955e1376576d6d709897b86369b0bb098e
Nickan <Nickan.bagherpour@gmail.com>
2021-12-07 11:22:25 +0330

    refactor: split WithSpinner component into two components [Spinner and WithSpinner]

--- src/components/with-spinner/with-spinner.component.jsx
+++ src/components/with-spinner/with-spinner.component.jsx
@@ -1,13 +1,9 @@
 import React from 'react';
 
-import {SpinnerContainer, SpinnerOverlay} from './with-spinner.styles';
-
 const WithSpinner = WrappedComponent => {
     const Spinner = ({isLoading, ...otherProps}) => {
         return isLoading ? (
-            <SpinnerOverlay>
-                <SpinnerContainer/>
-            </SpinnerOverlay>
+            <Spinner/>
         ) : (
             <WrappedComponent {...otherProps} />
         );
